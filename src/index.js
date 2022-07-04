4bee2a3b1c849025e8d3ca581e82f727e59aa0be
OlegSivyh <Oleg.sivyh@gmail.com>
2022-07-04 16:08:41 +0300

    added homework

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+
+  getActualFreePlace: function () {
+    return this.quantityOfPlaces - this.getLength();
+  },
+
+  paidPerPlace: function () {
+    return this.bankAccount + this.priceByPlace;
+  },
+
+  checkInGuest: function (firstName, secondName, money) {
+    if (this.getActualFreePlace() === 0) {
+      return 'Sorry, we have not free spaces';
+    } else if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    } else {
+      this.guests[this.getLength()] = {
+        firstName,
+        secondName,
+        money: money - this.priceByPlace,
+      };
+      this.paidPerPlace();
+    }
+  },
+};
